01000573bd14e477f29a65830bf13a3f43458f4c
liuqi008 <110798999+liuqi008@users.noreply.github.com>
2025-09-07 00:31:16 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace MirrorAudio
@@ -10,8 +12,54 @@ namespace MirrorAudio
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // TODO: 如果您有自定义启动窗体，替换 SettingsForm 为您的主窗体类名
-            Application.Run(new SettingsForm());
+
+            Form mainForm = null;
+            var asm = Assembly.GetExecutingAssembly();
+
+            // Try type MirrorAudio.SettingsForm or global SettingsForm
+            var t = asm.GetType("MirrorAudio.SettingsForm", throwOnError: false)
+                     ?? asm.GetType("SettingsForm", throwOnError: false);
+
+            if (t != null && typeof(Form).IsAssignableFrom(t))
+            {
+                try
+                {
+                    // Prefer ctor(AppSettings, Func<StatusSnapshot>)
+                    var ctor = t.GetConstructors()
+                                .FirstOrDefault(c =>
+                                {
+                                    var ps = c.GetParameters();
+                                    return ps.Length == 2
+                                        && ps[0].ParameterType.FullName == "MirrorAudio.AppSettings"
+                                        && ps[1].ParameterType.FullName.StartsWith("System.Func");
+                                });
+
+                    if (ctor != null)
+                    {
+                        var cur = new AppSettings(); // default settings
+                        Func<StatusSnapshot> provider = () => new StatusSnapshot { Running = false };
+                        mainForm = (Form)ctor.Invoke(new object[] { cur, provider });
+                    }
+                    else
+                    {
+                        // If there is a parameterless ctor, use it
+                        var ctor0 = t.GetConstructor(Type.EmptyTypes);
+                        if (ctor0 != null)
+                        {
+                            mainForm = (Form)Activator.CreateInstance(t);
+                        }
+                    }
+                }
+                catch
+                {
+                    mainForm = null;
+                }
+            }
+
+            // Fallback to placeholder if we couldn't construct
+            if (mainForm == null) mainForm = new GenericMainForm();
+
+            Application.Run(mainForm);
         }
     }
 }
